32ba6ba0102451392d94335d4101e1426e6165d5
Farmin Farzin <farminhoturner@gmail.com>
2020-01-31 14:38:44 +0100

    :fire: cleaning index  file

--- index.js
+++ index.js
@@ -19,18 +19,7 @@ async function run() {
 
     core.debug(`fetching changed files for pr #${prNumber}`);
     const changedFiles = await getChangedFiles(client, prNumber);
-    // const labelGlobs = await getLabelGlobs(
-    //   client,
-    //   configPath
-    // );
 
-    // const labels = [];
-    // for (const [label, globs] of labelGlobs.entries()) {
-    //   core.debug(`processing ${label}`);
-    //   if (checkGlobs(changedFiles, globs)) {
-    //     labels.push(label);
-    //   }
-    // }
     let depsChanged = false;
     for (const file of changedFiles) {
       if (file.includes("yarn.lock")) {
@@ -73,67 +62,7 @@ async function getChangedFiles(client, prNumber) {
   return changedFiles;
 }
 
-// async function getLabelGlobs(
-//   client: GitHub,
-//   configurationPath: string
-// ): Promise<Map<string, string[]>> {
-//   const configurationContent: string = await fetchContent(
-//     client,
-//     configurationPath
-//   );
 
-//   // loads (hopefully) a `{[label:string]: string | string[]}`, but is `any`:
-//   const configObject: any = yaml.safeLoad(configurationContent);
-
-//   // transform `any` => `Map<string,string[]>` or throw if yaml is malformed:
-//   return getLabelGlobMapFromObject(configObject);
-// }
-
-// async function fetchContent(
-//   client: GitHub,
-//   repoPath: string
-// ): Promise<string> {
-//   const response = await client.repos.getContents({
-//     owner: context.repo.owner,
-//     repo: context.repo.repo,
-//     path: repoPath,
-//     ref: context.sha
-//   });
-
-//   return Buffer.from(response.data.content, 'base64').toString();
-// }
-
-// function getLabelGlobMapFromObject(configObject: any): Map<string, string[]> {
-//   const labelGlobs: Map<string, string[]> = new Map();
-//   for (const label in configObject) {
-//     if (typeof configObject[label] === 'string') {
-//       labelGlobs.set(label, [configObject[label]]);
-//     } else if (configObject[label] instanceof Array) {
-//       labelGlobs.set(label, configObject[label]);
-//     } else {
-//       throw Error(
-//         `found unexpected type for label ${label} (should be string or array of globs)`
-//       );
-//     }
-//   }
-
-//   return labelGlobs;
-// }
-
-// function checkGlobs(changedFiles: string[], globs: string[]): boolean {
-//   for (const glob of globs) {
-//     core.debug(` checking pattern ${glob}`);
-//     const matcher = new Minimatch(glob);
-//     for (const changedFile of changedFiles) {
-//       core.debug(` - ${changedFile}`);
-//       if (matcher.match(changedFile)) {
-//         core.debug(` ${changedFile} matches`);
-//         return true;
-//       }
-//     }
-//   }
-//   return false;
-// }
 
 async function addLabel(client, prNumber) {
   await client.issues.addLabels({
